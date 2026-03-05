64193f920ba552ed502a52c8fcb3fc96492c324b
JamesRoyBoogaard <jamesboogaard66@gmail.com>
2026-03-05 09:50:18 +0100

    additions to the database_handler class and creation of sentencepair object class

--- Persistence/database_handler.py
+++ Persistence/database_handler.py
@@ -1,19 +1,31 @@
 import sqlite3
+import SentencePair
 
-def database_handler():
-    connection = sqlite3.connect("SentencePairs.db")
-    cursor = connection.cursor()
-    cursor.execute("Create Table If Not Exists sentence_pairs(ID INTEGER PRIMARY KEY, DutchSentence TEXT, EnglishSentence TEXT, LastReviewed DATETIME)")
-    connection.close()
+def __init__(self):
+    self.connection = sqlite3.connect("SentencePairs.db")
+    self.cursor = self.connection.cursor()
+    self.cursor.execute("Create Table If Not Exists sentence_pairs(ID INTEGER PRIMARY KEY AUTOINCREMENT, DutchSentence TEXT, EnglishSentence TEXT, LastReviewed DATETIME)")
+    
 
-def add_sentence_pair(sentence_pair):
+def add_sentence_pair(self, sentence_pair):
     # Add sentence_pair to the db
-    return sentence_pair
+    DutchSentence = sentence_pair.DutchSentence
+    EnglishSentence = sentence_pair.EnglishSentence
+    LastReviewed = sentence_pair.LastReviewed
 
-def remove_sentence_pair(sentence_pair_id):
+    new_tuple = (DutchSentence,EnglishSentence,LastReviewed)
+    self.cursor.execute("INSERT INTO sentence_pairs VALUES (?,?,?)", new_tuple)
+    self.connection.commit()
+    
+
+def remove_sentence_pair(self, sentence_pair_id):
     #remove the sentence pair from the database that has sentence_pair_id as its primary key
-    return sentence_pair_id
+    self.cursor.execute("DELETE FROM sentence_pairs WHERE ID = ?", sentence_pair_id)
+    self.connection.commit()
 
-def get_sentence_pairs(p_number_of_sentence_pairs):
+def get_sentence_pairs(self, p_number_of_sentence_pairs):
     # return p_number_of_sentence_pairs of the least revised sentence pairs as a List<SentencePairs>
     return p_number_of_sentence_pairs
+
+def close(self):
+    self.connection.close()
---
+++ SentencePair.py
@@ -0,0 +1,9 @@
+class SentencePair():
+    
+    def __init__(self, p_DutchSentence, p_EnglishSentence, p_LastReviewed, p_ID= None):
+        self.ID = p_ID
+        self.DutchSentence = p_DutchSentence
+        self.EnglishSentence = p_EnglishSentence
+        self.LastReviewed = p_LastReviewed
+    
+
