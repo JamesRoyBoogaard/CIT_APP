0c0ccbbd36518095102e282832041038a236bb1e
JamesRoyBoogaard <jamesboogaard66@gmail.com>
2026-03-12 15:58:20 +0100

    adding imports for using pyside6

--- GUI/cit_page.py
+++ GUI/cit_page.py
@@ -1,4 +1,9 @@
 from abc import ABC, abstractmethod
+from PySide6 import QtCore, QtWidgets, QtGui
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from Logic import logic_controller
 
 class CITPage(ABC):
 
