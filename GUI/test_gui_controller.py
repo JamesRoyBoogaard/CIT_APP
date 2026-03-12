c5ba789652efa35edbf5ac01ec04cd71ce39c07b
JamesRoyBoogaard <jamesboogaard66@gmail.com>
2026-03-12 11:55:42 +0100

    Doing actual tdd for the final section of the project

--- GUI/test_gui_controller.py
+++ GUI/test_gui_controller.py
@@ -1,1 +1,16 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import pytest
+
+# setup for the tests
+
+# gui_controller testing section
+
+# home_page testing section
+
+# add_sentences_pairs_page testing section
+
+# modify_sentence_pairs_page testing section
+
+# revision_page testing section
