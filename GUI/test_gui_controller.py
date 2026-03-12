307c57ab3fedc2bdddceaf0fc9b1889d398de146
JamesRoyBoogaard <jamesboogaard66@gmail.com>
2026-03-12 14:07:18 +0100

    adding all the test methods for gui

--- GUI/test_gui_controller.py
+++ GUI/test_gui_controller.py
@@ -7,10 +7,40 @@ import pytest
 
 # gui_controller testing section
 
+def test_run():
+    return False
+
 # home_page testing section
+
+def test_modify_sentence_page():
+    # assert that the window/scene/page has changed to the modify_sentence_page 
+    return False
+
+def test_revision_page():
+    # assert that the window/scene/page has changed to the revision_page
+    return False
+
+def test_add_sentence_pairs_page():
+    # assert that the window/scene/page has changed to the add_sentence_pairs_page
+    return False
 
 # add_sentences_pairs_page testing section
 
+def test_add_sentence_pairs():
+    return False
+
 # modify_sentence_pairs_page testing section
 
+def test_search_next():
+    return False
+
+def test_search_previous():
+    return False
+
+def test_modify_sentence_pairs():
+    return False
+
 # revision_page testing section
+
+def test_revision():
+    return False
