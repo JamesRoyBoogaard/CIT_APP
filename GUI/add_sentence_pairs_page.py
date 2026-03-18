66f2693cf2b243dbae459b7f42bd4b219ecde141
JamesRoyBoogaard <jamesboogaard66@gmail.com>
2026-03-18 16:15:12 +0100

    beginning to implement the add_sentence_class instead, new order of imp will be add, revision and modify

--- GUI/add_sentence_pairs_page.py
+++ GUI/add_sentence_pairs_page.py
@@ -5,16 +5,34 @@ class AddSentencePairsPage(QtWidgets.QWidget):
 
     def __init__(self, p_logic_controller = None):
         super().__init__()
-        self.resize(900,900)
+        self.setFixedSize(1200,900)
         if p_logic_controller == None:
             self.logic_controller = LogicController()
         else:
             self.logic_controller = p_logic_controller
-            
+
+
         self.heading = QtWidgets.QLabel("Add A Sentence Pair",
-                                alignment=QtCore.Qt.AlignmentFlag.AlignTop)
+                                     alignment=QtCore.Qt.AlignmentFlag.AlignTop)
+        self.previous_page_button = QtWidgets.QPushButton("Back")
+        self.home_button = QtWidgets.QPushButton("Home")
+        self.add_sentence_pair_button = QtWidgets.QPushButton("Add")
+        self.options_button = QtWidgets.QPushButton("Options")
+        
+
+        self.drop_down = QtWidgets.QMenu(self)
+        self.drop_down.addAction("Home", lambda: self.home_button.click())
+        self.drop_down.addAction("Sound",lambda: print("Sound has been toggled"))
+        self.options_button.setMenu(self.drop_down)
+
+
         self.layout = QtWidgets.QVBoxLayout(self)
         self.layout.addWidget(self.heading)
-        self.setLayout(self.layout)
+        self.layout.addWidget(self.previous_page_button)
+        self.layout.addWidget(self.options_button)
+        self.layout.addWidget(self.add_sentence_pair_button)
 
+    @QtCore.Slot()
+    def add_sentence_pair(self, p_sentence_pair):
+        self.logic_controller.add_sentence_pair(p_sentence_pair)
 
